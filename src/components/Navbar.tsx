0286a409460f67edbec5c0b586fbe0d951e8b5df
lucaszebre <yakirisk@gmail.com>
2023-08-06 06:28:15 +0200

    improve the css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,27 +23,29 @@ const Navbar = () => {
     return (
         <div className={styles.NavbarWrapper}>
             <header className={styles.Navbar}>
-                <div className={styles.NavbarImage}>
-                    <Image className={styles.LogoPc} src="/assets/images/logo-devlinks-large.svg" alt="devlink-logo" fill />
-                    <Image className={styles.LogoMobile} src="/assets/images/logo-devlinks-small.svg" alt="devlink-logo" fill />
-                </div>
-
-                <div className={styles.NavbarDiv}>
+                <div className={styles.Part1}>
+                    <div className={styles.NavbarImage}>
+                        <Image className={styles.LogoPc} src="/assets/images/logo-devlinks-large.svg" alt="devlink-logo" fill />
+                        <Image className={styles.LogoMobile} src="/assets/images/logo-devlinks-small.svg" alt="devlink-logo" fill />
+                    </div>
+                    <div className={styles.NavbarDiv}>
+                        <Button
+                        className={styles.ButtonLink}
+                            onClick={() => {
+                                setNavButton('Links');
+                            } }
+                            variant={Navbutton == 'Links' ? "contained" : "outlined"} startIcon={<CustomSVG choice='links' color={Navbutton == 'Links' ? "white" : "#633CFF"} />} >
+                        Links
+                    </Button>
                     <Button
                         onClick={() => {
-                            setNavButton('Links');
+                            setNavButton('Profile');
                         } }
-                        variant={Navbutton == 'Links' ? "contained" : "outlined"} startIcon={<CustomSVG choice='links' color={Navbutton == 'Links' ? "white" : "#633CFF"} />} >
-                    Links
-                </Button>
-                <Button
-                    onClick={() => {
-                        setNavButton('Profile');
-                    } }
-                    variant={Navbutton == "Profile" ? "contained" : "outlined"} startIcon={<CustomSVG choice='header' color={Navbutton == 'Profile' ? "white" : "#633CFF"} />}>
-                    Profile Details
-                </Button>
-            </div><div className={styles.LastDiv}>
+                        variant={Navbutton == "Profile" ? "contained" : "outlined"} startIcon={<CustomSVG choice='header' color={Navbutton == 'Profile' ? "white" : "#633CFF"} />}>
+                        Profile Details
+                    </Button>
+                                </div>
+                </div><div className={styles.LastDiv}>
                 <Button
                 variant='outlined'
                 >
