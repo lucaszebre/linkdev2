08e12f660a9014c51e0cf57408f124de45bb6879
lucaszebre <yakirisk@gmail.com>
2023-08-07 10:26:50 +0200

    change into supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Provider } from "@supabase/gotrue-js";
 
-const supabase = createClient('https://rrqmpgpvlhaxsozwlntq.supabase.co', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 
 export default supabase
