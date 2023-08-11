36289115d06ff7f3ceda8722710d08af8669b563
lucaszebre <yakirisk@gmail.com>
2023-08-11 03:52:09 +0200

    add first unit test

---
+++ src/__test__/Login.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import Login from '@/components/Login';
+import React from 'react'; // Make sure to import React
+
+// Mocking the Supabase client
+jest.mock('../../supabase', () => ({
+  auth: {
+    signInWithPassword: jest.fn(),
+    // Add other mock methods you're using in your tests
+  },
+  // Add other mocked Supabase client methods as needed
+}));
+describe('Login Component', () => {
+  it('Renders a "Login" button', () => {
+    render(<Login />);
+    const loginButton = screen.getByText(/Login with Google/);
+    expect(loginButton).toBeInTheDocument;
+  });
+
+  it('Renders email and password input fields', () => {
+    render(<Login />);
+    const emailInput = screen.getByPlaceholderText('e.g. lucasbeaugosse@email.com');
+    const passwordInput = screen.getByPlaceholderText('Enter your password');
+    expect(emailInput).toBeInTheDocument;
+    expect(passwordInput).toBeInTheDocument;
+  });
+
+  it('Renders "Login with Google" and "Login with Github" buttons', () => {
+    render(<Login />);
+    const googleButton = screen.getByRole('button', { name: 'Login with Google' });
+    const githubButton = screen.getByRole('button', { name: 'Login with Github' });
+    expect(googleButton).toBeInTheDocument;
+    expect(githubButton).toBeInTheDocument;
+  });
+
+  
+
+  // Add more tests for other functionalities and edge cases
+});
